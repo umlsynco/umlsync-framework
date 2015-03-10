cdc42461ecde4fda7dd44793554df17028873c42
Evgeny Alexeyev <ealekseev@smartlabs.tv>
2015-03-10 21:00:48 +0300

    Added menus.

--- js/Modules/Github/backend.js
+++ js/Modules/Github/backend.js
@@ -5,7 +5,7 @@ define(['marionette',
     function (Marionette, Framework, Github) {
         Framework.module('Backend', function(Backend) {
             Backend.Github = {};
-            Backend.Github = new Github({username:'umlsynco', token:"bcefc8ea4d8b23ab9d1d0436b5999616cde9b040", singleton: true});
+            Backend.Github = new Github({username:'umlsynco', token:"f7b6588dbb5b2d70c3c1c9101ef00451b71200cd", singleton: true});
         });
         return Framework.Backend.Github;
     });
