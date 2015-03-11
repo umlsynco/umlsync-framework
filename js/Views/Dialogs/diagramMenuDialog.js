be991b8cf8a69bd85284341e004a4f774190fce4
Evgeny Alexeyev <ealekseev@smartlabs.tv>
2015-03-11 13:17:32 +0300

    Extended menu with lists.

--- js/Views/Dialogs/diagramMenuDialog.js
+++ js/Views/Dialogs/diagramMenuDialog.js
@@ -20,6 +20,26 @@ define([
 			addAccordionItem: function() {
 				// Extend an accordion structure with a new item
 				var model = {type: "class", title: "Class"};
+
+				if (this.count == 0) {
+   				  // Extend an accordion structure with a new item
+				  var model = {type: "package", title: "Package"};
+                  this.count++;
+				}
+				else if (this.count == 1) {
+   				  // Extend an accordion structure with a new item
+				  var model = {type: "component", title: "Component"};
+                  this.count++;
+				}
+				else if (this.count == 2) {
+   				  // Extend an accordion structure with a new item
+				  var model = {type: "sequence", title: "Sequence"};
+                  this.count++;
+				}
+				else {
+					this.count = 0;
+				}
+
 				var that = this;
 				
 				$.ajax({url: "./assets/menu/ds/"+model.type+"_with_menu.json", dataType: "json", success:function(data) {
@@ -30,8 +50,6 @@ define([
             onShow: function() {
 				//if (this.accordion) return;
 				this.accordion = new Accordion({collection: new Backbone.Collection()});
-				this.accordion.collection.add({title:"gggg"});
-				this.accordion.collection.add({title:"hhhh"});
 				this.getRegion('accordion').show(this.accordion);
 				var that = this;
 
