27eaab57d566390ed163a9765d79bb3afd9962a2
Evgeny Alexeyev <evgeny.alexeyev@gmail.com>
2015-12-16 19:21:22 +0300

    Fixed class diagram menu issues. TODO: Sequence diagram handling issues ????

--- js/Modules/Diagrammer/Controllers/SequenceElementDropController.js
+++ js/Modules/Diagrammer/Controllers/SequenceElementDropController.js
@@ -22,66 +22,66 @@ define(['marionette',
 
             },
             onElementAdd: function(element) {
-				// Do nothing for the temporary elements (DND)
-				if (element.model.get("temporary")) return;
-				// avoid iteration for the not droppable elements
-				if ((!element.droppable) && (element.acceptDrop.length == 0)) return;
+                // Do nothing for the temporary elements (DND)
+                if (element.model.get("temporary")) return;
+                // avoid iteration for the not droppable elements
+                if ((!element.droppable) && (element.acceptDrop.length == 0)) return;
 
-				if (element.droppedElements.length >0)
-				  //TODO: Work-around. There is no way to get dropped elements before new element creation
-				  //      but somhow it is not empty. Most likely it is an issue of backbone or marionette
-				  element.droppedElements = new Array();
+                if (element.droppedElements.length >0)
+                  //TODO: Work-around. There is no way to get dropped elements before new element creation
+                  //      but somhow it is not empty. Most likely it is an issue of backbone or marionette
+                  element.droppedElements = new Array();
 
-				this.elements.children.each(function(child) {
-				  if (child != element) {
-					  element.dropDone(child);
-				  }
-				});
+                this.elements.children.each(function(child) {
+                  if (child != element) {
+                      element.dropDone(child);
+                  }
+                });
 
-				if (element.droppedElements.length >0) {
-  				    $.log("NAME: " + element.model.get("type"));
-  				}
-  				else if (element.model.get("type") == "llport") {
-					if (element.dropParent == null) {
-						element.model.collection.add(new Backbone.DiagramModel({name:"Class2",type:"objinstance", left: (element.model.get("left") - 100), "width":150, "height": 250}));
-					}
-				}
-			},
+                if (element.droppedElements.length >0) {
+                      $.log("NAME: " + element.model.get("type"));
+                  }
+                  else if (element.model.get("type") == "llport") {
+                    if (element.dropParent == null) {
+                        element.model.collection.add(new Backbone.DiagramModel({name:"Class2",type:"objinstance", left: (element.model.get("left") - 100), "width":150, "height": element.model.get("top") + 150}));
+                    }
+                }
+            },
             skipOneSelect: false,
             onElementSelect: function(itemView, event) {
-				// Skip selection DND completion
-				if (this.skipOneSelect) {
-					this.skipOneSelect = false;
-					return;
-				}
+                // Skip selection DND completion
+                if (this.skipOneSelect) {
+                    this.skipOneSelect = false;
+                    return;
+                }
 
-				if (event && event.ctrlKey) {
-					itemView.onSelect(!itemView.selected);
-					return;
-				}
-				var that = this;
+                if (event && event.ctrlKey) {
+                    itemView.onSelect(!itemView.selected);
+                    return;
+                }
+                var that = this;
                 _.each(this.elements.children._views, function(item) {
                    if (item != itemView) {
                        item.onSelect(false);
                    }
                 });
                 itemView.onSelect(true);
-			},
-			onNewConnector: function(connector) {
-				if (connector.model.get("temporary")) return;
+            },
+            onNewConnector: function(connector) {
+                if (connector.model.get("temporary")) return;
 
-				var fid = connector.model.get("fromId");
-				var tid = connector.model.get("toId");
-				$.log("LF: " + tid + " ; " + fid);
-				this.elements.children.each(function(item) {
-					$.log("CHILD: " + item.model.cid);
-					if (item.model.get("id") == fid) {
-						fid = item;
-					}
-					else if (item.model.get("id") == tid) {
-						tid = item;
-					}
-				});
+                var fid = connector.model.get("fromId");
+                var tid = connector.model.get("toId");
+                $.log("LF: " + tid + " ; " + fid);
+                this.elements.children.each(function(item) {
+                    $.log("CHILD: " + item.model.cid);
+                    if (item.model.get("id") == fid) {
+                        fid = item;
+                    }
+                    else if (item.model.get("id") == tid) {
+                        tid = item;
+                    }
+                });
 
 // tOdO:
 // 1. always create epoint for the sequence connectors
@@ -91,17 +91,17 @@ define(['marionette',
 // 3. if llport:
 // 3.1 If dropped out of port => 2.1 & 2.2
 
-				if (fid.model && (fid.model.get("type") == "objinstance")) {
-					var y = connector.toModel.get("top");
-					var fromModel = new Backbone.DiagramModel({type:"llport", width:15, height:25, left: fid.model.get("left") + fid.model.get("width")/2, top: y});
-					this.elements.collection.add(fromModel);
-					connector.fromModel = fromModel;
-					connector.model.set({fromId: fromModel.get("id")});
-				}
-				
-				// 1. If connector from objinstance then create from port or attach to existing one
-				// 2. if connector dropped on objinstance then crea llport and drop it on
-			},
+                if (fid.model && (fid.model.get("type") == "objinstance")) {
+                    var y = connector.toModel.get("top");
+                    var fromModel = new Backbone.DiagramModel({type:"llport", width:15, height:25, left: fid.model.get("left") + fid.model.get("width")/2, top: y});
+                    this.elements.collection.add(fromModel);
+                    connector.fromModel = fromModel;
+                    connector.model.set({fromId: fromModel.get("id")});
+                }
+                
+                // 1. If connector from objinstance then create from port or attach to existing one
+                // 2. if connector dropped on objinstance then crea llport and drop it on
+            },
             onConnectorDragStart: function(conView, ui) {
                 this.dragAlsoElements = new Array();
                 this.draggableConnectors = new Array();
@@ -117,21 +117,21 @@ define(['marionette',
                 _.each(this.dragAlsoElements, function(element, idx) {
                    element.onDragStart(ui);
                 });
-			},
-			onConnectorDragDo: function(conView) {
-				// use common dragDo
-			},
-			onConnectorDragStop: function(conView, ui) {
-				
-				//////////////////////////// COMPLETE DROP OF EACH ELEMENT AND SYNC UP MODELS
+            },
+            onConnectorDragDo: function(conView) {
+                // use common dragDo
+            },
+            onConnectorDragStop: function(conView, ui) {
+                
+                //////////////////////////// COMPLETE DROP OF EACH ELEMENT AND SYNC UP MODELS
                 _.each(this.dragAlsoElements, function(element, idx) {
                    element.onDragStop(ui);
                 });
 
                 // Sync up epoints on drag stop
                 _.each(this.draggableConnectors, function(connector, idx) {
-					connector.onDragStop(ui);
-				});
+                    connector.onDragStop(ui);
+                });
 
                 // Refresh droppable relations
                 var that = this;
@@ -145,25 +145,25 @@ define(['marionette',
 
                 this.dragAlsoElements = new Array();
                 this.draggableConnectors = new Array();
-			},
+            },
             // Droppable
             dragAlsoElements: [],
             draggableConnectors: [],
             onDragDo: function(itemView, ui) {
-				if (ui && ui.x)
-				  $.log("DRAG DO " + ui.x + " Y: " + ui.y);
+                if (ui && ui.x)
+                  $.log("DRAG DO " + ui.x + " Y: " + ui.y);
                 _.each(this.dragAlsoElements, function(element, idx) {
                     if (element != itemView)
                         element.onDragDo(ui);
                 });
 
                 _.each(this.draggableConnectors, function(connector, idx) {
-					connector.onDragDo(ui);
-				});
+                    connector.onDragDo(ui);
+                });
             },
             onDragStart: function(itemView, ui) {
-				// Skip one select on DND completion
-				this.skipOneSelect = true;
+                // Skip one select on DND completion
+                this.skipOneSelect = true;
 
                 //this.dragAlsoElements = itemView.$el.parent().find('.dropped-' + itemView.cid);
                 this.dragAlsoElements = new Array();
@@ -171,10 +171,10 @@ define(['marionette',
 
                 // Do not support drag also for the not dropped connector
                 if (itemView.model.get("type") == "llport" && itemView.dropParent == null) {
-					// Emulate element select, to prevent wrong expectation from user
-					this.onElementSelect(itemView, {ctrlKey:false});
-					return;
-				}
+                    // Emulate element select, to prevent wrong expectation from user
+                    this.onElementSelect(itemView, {ctrlKey:false});
+                    return;
+                }
                 
                 var queued = new Array();
                 queued.push(itemView.model.cid);
@@ -186,12 +186,12 @@ define(['marionette',
                 
                 this.elements.children.each(function(item) {
                    if (item != itemView && item.selected) {
-					        
-					   if ((isLlport && item.model.get("type") == "llport")
-					     || (isObjInstance && item.model.get("type") != "llport")) {
+                            
+                       if ((isLlport && item.model.get("type") == "llport")
+                         || (isObjInstance && item.model.get("type") != "llport")) {
                          that.dragAlsoElements.push(item);
                          queued.push(item.model.cid);
-					 }
+                     }
                    }
                 });
 
@@ -204,46 +204,46 @@ define(['marionette',
                 });
                 
                 if (isLlport) {
-					var secondStageModels = new Array();
-					// Check outgoing connectors
+                    var secondStageModels = new Array();
+                    // Check outgoing connectors
                     this.connectors.children.each(function(connector, idx) {
-	    				if (connector.fromModel == itemView.model) {
-							secondStageModels.push(connector.toModel);
-						}
-						else if (connector.toModel == itemView.model) {
-							secondStageModels.push(connector.fromModel);
-						}
-					});
-					$.log("OUTGOING: " + secondStageModels.length);
-					if (secondStageModels.length > 0) {
-						var nextStageModels = new Array();
-						_.each(secondStageModels, function(m4) {
-							var hasMoreConnectors = false;
-						   that.connectors.children.each(function(c4) {
-							   if ((c4.fromModel == m4 && c4.toModel != itemView.model)
-							     || (c4.toModel == m4 && c4.fromModel != itemView.model)) {
-							     hasMoreConnectors = true;
-							   }
-						   });
-						   if (!hasMoreConnectors) {
-							   nextStageModels.push(m4);
-						   }
-					   });
+                        if (connector.fromModel == itemView.model) {
+                            secondStageModels.push(connector.toModel);
+                        }
+                        else if (connector.toModel == itemView.model) {
+                            secondStageModels.push(connector.fromModel);
+                        }
+                    });
+                    $.log("OUTGOING: " + secondStageModels.length);
+                    if (secondStageModels.length > 0) {
+                        var nextStageModels = new Array();
+                        _.each(secondStageModels, function(m4) {
+                            var hasMoreConnectors = false;
+                           that.connectors.children.each(function(c4) {
+                               if ((c4.fromModel == m4 && c4.toModel != itemView.model)
+                                 || (c4.toModel == m4 && c4.fromModel != itemView.model)) {
+                                 hasMoreConnectors = true;
+                               }
+                           });
+                           if (!hasMoreConnectors) {
+                               nextStageModels.push(m4);
+                           }
+                       });
 
- 					   that.elements.children.each(function(e4) {
-						   _.each(nextStageModels, function(ns) {
-							   if (e4.model == ns) {
-								   if (!_.contains(that.dragAlsoElements, e4)) {
-								     that.dragAlsoElements.push(e4);
-								     queued.push(e4.model.cid);
-								   }
-							   }
-						   });
-					   });
+                        that.elements.children.each(function(e4) {
+                           _.each(nextStageModels, function(ns) {
+                               if (e4.model == ns) {
+                                   if (!_.contains(that.dragAlsoElements, e4)) {
+                                     that.dragAlsoElements.push(e4);
+                                     queued.push(e4.model.cid);
+                                   }
+                               }
+                           });
+                       });
 
-					}
-					
-				}
+                    }
+                    
+                }
 
                 // Include all containments
                 _.each(this.dragAlsoElements, function(e2) {
@@ -260,13 +260,13 @@ define(['marionette',
 
                 // Trigger drag start
                 this.connectors.children.each(function(connector, idx) {
-					if (queued.indexOf(connector.fromModel.cid) >= 0
-					  && queued.indexOf(connector.toModel.cid) >= 0) {
-				      // Connector drag start
+                    if (queued.indexOf(connector.fromModel.cid) >= 0
+                      && queued.indexOf(connector.toModel.cid) >= 0) {
+                      // Connector drag start
                       connector.onDragStart(ui);
                       // Push connectors
                       that.draggableConnectors.push(connector);
-			        }
+                    }
                 });
 
             },
@@ -278,8 +278,8 @@ define(['marionette',
                 });
                 // Sync up epoints on drag stop
                 _.each(this.draggableConnectors, function(connector, idx) {
-					connector.onDragStop(ui);
-				});
+                    connector.onDragStop(ui);
+                });
 
                 //
                 // Check the droppable relation for the dragged element
--- js/Modules/Diagrammer/Views/Elements/umlobjinstance.js
+++ js/Modules/Diagrammer/Views/Elements/umlobjinstance.js
@@ -4,7 +4,7 @@ define(['marionette'],
             template: _.template('<div id="<%=cid%>" class="us-element-resizable-area" style="width:100%;">\
         <div class="us-instance-line"></div>\
         <div id="<%=cid%>_NEXT" class="us-instance grElement" style="height:40px;">\
-        <div><a id="name" class="editablefield Name">:<%=name%></a></div></div></div>'),
+        <div>:<a id="name" class="editablefield Name"><%=name%></a></div></div></div>'),
             axis: "x",
             acceptDrop: ["llport", "lldel"],
             behaviors: {
@@ -13,6 +13,39 @@ define(['marionette'],
                     "axis": "x"
                 },
                 EditableBehavior: {}
+            },
+            _checkRelation: function(e1, e2) {
+                // e1 could be dropped on e2
+                if (e1.droppable && _.contains(e2.acceptDrop, e1.model.get("type"))) {
+                    var inner = {
+                        left: e1.model.get("left"),
+                        top: e1.model.get("top"),
+                        width: e1.model.get("width"),
+                        height: e1.model.get("height")
+                        },
+                    outer = {
+                        left: e2.model.get("left"),
+                        top: e2.model.get("top"),
+                        width: e2.model.get("width"),
+                        height: e2.model.get("height")
+                    };
+                    if (outer.left < inner.left && outer.left + outer.width > inner.left + inner.width) {
+                        e2.$el.addClass('dropped-' + e1.cid);
+                        if (!_.contains(e2.droppedElements, e1)) {
+                            e2.droppedElements.push(e1);
+                        }
+                        if (outer.top < inner.top && inner.top + inner.height > outer.top + outer.height) {
+							this.$el.css("height", inner.top + inner.height);
+					    }
+                        
+                        return true;
+                    }
+                    else {
+                        e2.droppedElements = _.without(e2.droppedElements, e1);
+                        e2.$el.removeClass('dropped-' + e1.cid);
+                    }
+                }
+                return false;
             },
             dropDone: function(dev) {
                 // Check if this element could be dropped on dev
